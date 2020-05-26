b62f8538c65169c2300ccd9ff4c5eef82722d077
8q <yggdfolium@gmail.com>
2020-05-26 22:37:47 +0800

    bug fix

--- js/treeGraph1.js
+++ js/treeGraph1.js
@@ -28,8 +28,8 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
     const myDict = {}
     myDict.name = 'all'
     myDict.children = transform(PhylumClassOrderFamilyGenusSpecies)
-
-    const treewidth = document.body.clientWidth * 0.85
+    const windowWidth = window.innerWidth
+    const treewidth = windowWidth * 0.85
     const treePadding = { top: 10, right: 20, bottom: 10, left: 20 }
     const tdx = 20
     const tdy = treewidth / 15
@@ -57,6 +57,7 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
       const svg1 = d3
         .create('svg')
         .attr('viewBox', [-treePadding.left, -treePadding.top, treewidth, tdx])
+        .attr('width', treewidth)
         .style('font', '10px sans-serif')
         .style('user-select', 'none')
 
@@ -269,8 +270,8 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
     const timeBegin = d3.min(finalData, (d) => d.start_year)
     const timeEnd = d3.max(finalData, (d) => d.end_year)
 
-    const m = [15, 15, 15, 45] // top right bottom left
-    const w = document.body.clientWidth / 2 - 10 - m[1] - m[3]
+    const m = [10, 15, 15, 45] // top right bottom left
+    const w = windowWidth / 2 - 10 - m[1] - m[3]
 
     // 可改动
     const miniHeight = laneLength * 12 + 50

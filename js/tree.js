b5fa2c20d7f553487ed53fa6ad0164f724fd6547
qiaoyufei <17822000907@163.com>
2020-06-03 21:31:23 +0800

    select data

--- js/tree.js
+++ js/tree.js
@@ -2,7 +2,7 @@
 Promise.all([barGraphPromise, reHighlightPromise]).then(
   ([[finalData, PhylumClassOrderFamilyGenusSpecies], reHighlight]) => {
     // barGraphPromise.then(([finalData, PhylumClassOrderFamilyGenusSpecies, datum]) => {
-    const height = $('#tree').height()-30
+    const height = $('#tree').height()
     const width = $('#tree').width()
 
     function transform(node) {
@@ -21,7 +21,7 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
 
     const partition = (data) => {
       const root = d3.hierarchy(data).count()
-      return d3.partition().size([height, ((root.height + 1) * width) / 3])(root)
+      return d3.partition().size([height, ((root.height + 1) * width) / 2])(root)
     }
 
     const chart = (() => {
@@ -149,7 +149,7 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
     document.getElementById('tree').appendChild(chart)
 
     // 画timeline图
-    const heightT = 19000
+    const heightT = $('#time').height()
     const widthT = $('#time').width()
     const marginT = { top: 30, right: 30, bottom: 20, left: 10 }
 
@@ -187,6 +187,12 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
         .style('font', '11px sans-serif')
     }
 
+    const formatDate = (d) => (d < 0 ? `${-d}MA` : `${d}AD`)
+
+    const axisTop = d3.axisTop(xT).tickPadding(2).tickFormat(formatDate)
+
+    const axisBottom = d3.axisBottom(xT).tickPadding(2).tickFormat(formatDate)
+
     const getRect = function (d) {
       const el = d3.select(this)
       const sx = xT(d.start_year)
@@ -203,18 +209,51 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
         .text(d.Species)
     }
 
-    const formatDate = (d) => (d < 0 ? `${-d}MA` : `${d}AD`)
+    const sortName = ['Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
 
-    const axisTop = d3.axisTop(xT).tickPadding(2).tickFormat(formatDate)
+    function reDrawBar(nodeNameList, nodeDepth) {
+      let newData = finalData.filter((d) => {
+        var boolValue = true
+        for (var i = 0; i < nodeDepth; i++) {
+          if (d[sortName[i]] != nodeNameList[i]) {
+            boolValue = false
+          }
+        }
+        return boolValue
+      })
 
-    const axisBottom = d3.axisBottom(xT).tickPadding(2).tickFormat(formatDate)
+      let nextNameSet = new Set()
+      newData.forEach((element) => {
+        nextNameSet.add(element[sortName[nodeDepth]])
+      })
 
-    const reDrawColor=['red','orange','yellow','green','blue','purple']
-    function reDrawBar(nodeNameList, nodeDepth){
-      if(nodeDepth==1){
-        var phylum=nodeNameList[0]
-        var chooseRect = d3.selectAll(`.civ[data-phylum="${phylum}"]`)
-        chooseRect.select('rect').attr('fill', reDrawColor[nodeDepth-1])
+      let nextNameList = [...nextNameSet]
+      console.log(nextNameList)
+
+      let nextItemList = []
+
+      for (var i = 0; i < nextNameList.length; i++) {
+        let obj = {}
+        let min, max
+        let element = nextNameList[i]
+        obj['name'] = element
+        let selectData = newData.filter((d) => {
+          return element == d[sortName[nodeDepth]]
+        })
+        for (var j = 0; j < selectData.length; j++) {
+          element1 = selectData[j]
+          if (j == 0) {
+            min = element1['start_year']
+            max = element1['end_year']
+            console.log(min, max)
+          } else {
+            min = Math.min(min, element1['start_year'])
+            max = Math.max(max, element1['end_year'])
+          }
+        }
+        obj['start_year'] = min
+        obj['end_year'] = max
+        nextItemList.push(obj)
       }
     }
 
@@ -246,13 +285,6 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
         .attr('data-species', (d) => d.Species)
 
       const tooltip = d3.select(document.createElement('div')).call(createTooltip)
-
-      const line = svg
-        .append('line')
-        .attr('y1', marginT.top - 10)
-        .attr('y2', heightT - marginT.bottom)
-        .attr('stroke', 'rgba(0,0,0,0.2)')
-        .style('pointer-events', 'none')
 
       groups.attr('transform', (d, i) => `translate(0 ${yT(d.lane)})`)
 
@@ -288,6 +320,29 @@ Promise.all([barGraphPromise, reHighlightPromise]).then(
         .attr('transform', (d, i) => `translate(0 ${yT(d.lane)})`)
       return parent
     })()
+
+    // const chartT = (() => {
+    //   const parent = document.createElement('div')
+
+    //   const svg = d3.select(DOM.svg(widthT + 200, heightT))
+
+    //   const g = svg
+    //     .append('g')
+    //     .attr('transform', (d, i) => `translate(${marginT.left} ${marginT.top})`)
+
+    //   svg
+    //     .append('g')
+    //     .attr('transform', (d, i) => `translate(${marginT.left} ${marginT.top - 10})`)
+    //     .call(axisTop)
+
+    //   svg
+    //     .append('g')
+    //     .attr('transform', (d, i) => `translate(${marginT.left} ${heightT - marginT.bottom})`)
+    //     .call(axisBottom)
+
+    //   parent.appendChild(svg.node())
+    //   return parent
+    // })()
 
     document.getElementById('time').appendChild(chartT)
   },
